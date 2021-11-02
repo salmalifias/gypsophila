645c45b9ddd04d2a49cf6cf47819c65e998ceff7
Sharashena Chairani <shenachairani@gmail.com>
2021-11-02 14:57:03 +0700

    Tambah Link Portfolio di Muartist

--- src/pages/Muartist.js
+++ src/pages/Muartist.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import HeaderDark from "parts/HeaderDark";
 import Footer from "parts/Footer";
 import styled from "styled-components";
-import Card from "parts/Card";
+import CardMuartist from "parts/CardMuartist";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 //import { GrLocation } from "react-icons/gi";
@@ -26,6 +26,15 @@ const Title = styled.h1`
     font-weight: 500;
     margin-bottom: 45px;
 `;
+const a = styled.a`
+    text-transform:uppercase;
+    color: #189CFC;
+    text-decoration:none;
+    font-weight:bold;
+    margin-right:100px;
+    align-items: center;
+    justify-content:center; 
+`;
 
     return (
       <>
@@ -35,55 +44,56 @@ const Title = styled.h1`
             <Title>Makeup Artist</Title>
                 <Row xs="auto" className="g-4">
                   <Col lg={true}>
-                    <Card className="m-5 d-block text-center"
+                    <CardMuartist className="m-5 d-block text-center"
                       title='By Jessica'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache/user/5679/b71985397688d6f1820685dde534981b-350-200.jpg'
-                      body='Jakarta Pusat' />
+                      body='Jakarta Pusat'
+                      />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Lyn.MUA'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache//user/16410/63f5b021c36fcedfcb08c3de302b90f8-350-200.jpeg'
                       body='Jakarta Selatan' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Clara Kim'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache/user/1153/d30217d1cbf2559a9894e09a56ad8d44-350-200.jpeg'
                       body='Jakarta Utara' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Ririn Makeup Artist'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache/user/4041/2bd02dead545292f2daa41870a6b601f-350-200.jpeg'
                       body='Jakarta Utara' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
-                      title='Salma Bridal Makeup'
+                    <CardMuartist className="m-5 d-block"
+                      title='Salma Bridal MUA'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache/user/8795/70e3fbbb11dd5e37f1dbfd6a3c16bdec-350-200.jpg'
                       body='Jakarta Barat' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Glow Like Lady'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache//user/17386/6f0507da6ab25264398d14a0ba7e35f2-350-200.jpeg'
                       body='Jakarta Timur' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Levina.Muart'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache//user/15674/b1764ab4b800ec43aa0934ff5b360bd7-350-200.jpeg'
                       body='Jakarta Selatan' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Say Lalisa'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache//user/14123/1898012a21048f8fbeff602ed63d98d0-350-200.jpeg'
                       body='Jakarta Timur' />
                   </Col>
                   <Col lg={true}>
-                    <Card className="m-5 d-block"
+                    <CardMuartist className="m-5 d-block"
                       title='Beauty by Rose'
                       imageUrl='https://www.hellobeauty.id/storage/images/cache/user/6861/71e8b5091eb9c4b3702c3961a1580d4f-350-200.jpeg'
                       body='Jakarta Selatan' />
---
+++ src/parts/CardMuartist.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'parts/Card.css';
+import { Link } from 'react-router-dom';
+
+function Card({title,imageUrl,subtitle,body}){
+    return (
+        <div className='card-container'>
+            <div className="image-container">
+                <img src={imageUrl} alt='' />
+            </div>
+            <div className="card-content">
+            <div className="card-title">
+                <h4 className="text-center">{title}</h4>
+            </div>
+            <div className="card-subtitle">
+            <p className="mb-2 text-muted text-center">{subtitle}</p>
+            </div>
+            <div className="card-body">
+            <h6 className="text-center">{body}</h6>
+            </div>
+            <div className="btn">
+                <Link className="mx-auto" type="link" to={{ pathname: "https://drive.google.com/drive/folders/1pV7OXCNUuk_yGUpYTAp7jkJeGzLkRi7g?usp=sharing" }} target="_blank">
+                <button>
+                    <a>
+                        View Portfolio
+                    </a>
+                </button>
+                </Link>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
