b71941f1799948601aca3a8c02205ede0a5d7582
salmalifias <47998386+salmalifias@users.noreply.github.com>
2021-12-13 12:03:01 +0700

    salma

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4UjufyvltfF7ppYlDUYzlPG1gD3MfgV0",
+  authDomain: "gypsophila-a7a0d.firebaseapp.com",
+  projectId: "gypsophila-a7a0d",
+  storageBucket: "gypsophila-a7a0d.appspot.com",
+  messagingSenderId: "183713990383",
+  appId: "1:183713990383:web:c285f41fff4b8fb3a4fd1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -18,6 +18,13 @@ import Completed from "parts/Checkout/Completed";
 //import ItemDetails from "json/itemDetails.json";
 
 import { submitBooking } from "store/actions/checkout";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../firebase";
 
 class Checkout extends Component {
   state = {
@@ -26,7 +33,7 @@ class Checkout extends Component {
       lastName: "",
       email: "",
       phone: "",
-      proofPayment: "",
+      proofPayment: null,
       bankName: "",
       bankHolder: "",
     },
@@ -48,6 +55,43 @@ class Checkout extends Component {
   _Submit = (nextStep) => {
     const { data } = this.state;
     const { checkout } = this.props;
+
+    const fileName = new Date().getTime() + data.proofPayment[0].name;
+    const storage = getStorage(app)
+    const storageRef = ref(storage,fileName)
+    const uploadTask = uploadBytesResumable(storageRef, data.proofPayment[0]);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+            default:
+        }
+      }, 
+      (error) => {
+        // Handle unsuccessful uploads
+      }, 
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
 
     const payload = new FormData();
     payload.append("firstName", data.firstName);
@@ -71,6 +115,7 @@ class Checkout extends Component {
   render() {
     const { data } = this.state;
     const { checkout, page } = this.props;
+    console.log(data.proofPayment)
     if (!checkout)
       return (
         <div className="container">
